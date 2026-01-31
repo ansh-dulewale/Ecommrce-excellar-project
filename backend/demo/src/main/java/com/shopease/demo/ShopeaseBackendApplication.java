dee99fce45c4098ab7097608ec3bb3c0dcfa0021
Ansh Dulewale <120741334+ansh-dulewale@users.noreply.github.com>
2026-01-31 11:05:03 +0530

    First Set Up

---
+++ backend/demo/src/main/java/com/shopease/demo/ShopeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopease.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeaseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/demo/src/test/java/com/shopease/demo/ShopeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopease.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
